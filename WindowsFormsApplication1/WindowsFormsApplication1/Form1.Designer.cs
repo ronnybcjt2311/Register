e1ef8082e48f70e98a058c4b9a767eadec8c45a8
ronnybcjt2311 <ronnybcjt@gmail.com>
2018-09-11 23:44:24 +0700

    Add file to git

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Register));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.cancel = new System.Windows.Forms.Button();
+            this.accept = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.userName = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.cfpassword = new System.Windows.Forms.TextBox();
+            this.idName = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(224, 729);
+            this.panel1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.Location = new System.Drawing.Point(12, 618);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(201, 99);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Back";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.Location = new System.Drawing.Point(12, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(201, 99);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Login";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel4.Controls.Add(this.pictureBox1);
+            this.panel4.Controls.Add(this.label1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(220, 128);
+            this.panel4.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(81, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.label1.Location = new System.Drawing.Point(48, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "OxGame";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.cancel);
+            this.panel2.Controls.Add(this.accept);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(224, 571);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(798, 158);
+            this.panel2.TabIndex = 1;
+            // 
+            // cancel
+            // 
+            this.cancel.FlatAppearance.BorderColor = System.Drawing.Color.DarkTurquoise;
+            this.cancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cancel.ForeColor = System.Drawing.Color.Salmon;
+            this.cancel.Location = new System.Drawing.Point(429, 35);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(195, 73);
+            this.cancel.TabIndex = 1;
+            this.cancel.Text = "Cancel";
+            this.cancel.UseVisualStyleBackColor = true;
+            // 
+            // accept
+            // 
+            this.accept.FlatAppearance.BorderColor = System.Drawing.Color.DarkTurquoise;
+            this.accept.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.accept.ForeColor = System.Drawing.Color.LimeGreen;
+            this.accept.Location = new System.Drawing.Point(156, 35);
+            this.accept.Name = "accept";
+            this.accept.Size = new System.Drawing.Size(195, 73);
+            this.accept.TabIndex = 0;
+            this.accept.Text = "Accept";
+            this.accept.UseVisualStyleBackColor = true;
+            this.accept.Click += new System.EventHandler(this.accept_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel3.Controls.Add(this.dataGridView1);
+            this.panel3.Controls.Add(this.pictureBox2);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(224, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(798, 130);
+            this.panel3.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(773, 33);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(10, 10);
+            this.dataGridView1.TabIndex = 11;
+            this.dataGridView1.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(185, 46);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.label2.Location = new System.Drawing.Point(255, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(277, 79);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Register";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.label3.Location = new System.Drawing.Point(375, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 30);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Username";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.label4.Location = new System.Drawing.Point(383, 307);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 30);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Password";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.label5.Location = new System.Drawing.Point(281, 376);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(224, 30);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Confirm-Password";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.label6.Location = new System.Drawing.Point(420, 435);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 30);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Name";
+            // 
+            // userName
+            // 
+            this.userName.AccessibleDescription = "";
+            this.userName.Location = new System.Drawing.Point(541, 245);
+            this.userName.Name = "userName";
+            this.userName.Size = new System.Drawing.Size(256, 27);
+            this.userName.TabIndex = 7;
+            this.userName.Tag = "";
+            // 
+            // password
+            // 
+            this.password.AccessibleDescription = "";
+            this.password.Location = new System.Drawing.Point(541, 312);
+            this.password.Name = "password";
+            this.password.PasswordChar = '*';
+            this.password.Size = new System.Drawing.Size(256, 27);
+            this.password.TabIndex = 8;
+            this.password.Tag = "";
+            // 
+            // cfpassword
+            // 
+            this.cfpassword.AccessibleDescription = "";
+            this.cfpassword.Location = new System.Drawing.Point(541, 381);
+            this.cfpassword.Name = "cfpassword";
+            this.cfpassword.PasswordChar = '*';
+            this.cfpassword.Size = new System.Drawing.Size(256, 27);
+            this.cfpassword.TabIndex = 9;
+            this.cfpassword.Tag = "";
+            // 
+            // idName
+            // 
+            this.idName.AccessibleDescription = "";
+            this.idName.Location = new System.Drawing.Point(541, 440);
+            this.idName.Name = "idName";
+            this.idName.Size = new System.Drawing.Size(256, 27);
+            this.idName.TabIndex = 10;
+            this.idName.Tag = "";
+            // 
+            // Register
+            // 
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(1022, 729);
+            this.Controls.Add(this.idName);
+            this.Controls.Add(this.cfpassword);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.userName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(120)))), ((int)(((byte)(134)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Register";
+            this.panel1.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        internal System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox userName;
+        private System.Windows.Forms.Button cancel;
+        private System.Windows.Forms.Button accept;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.TextBox cfpassword;
+        private System.Windows.Forms.TextBox idName;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,120 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Register : Form
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void accept_Click(object sender, EventArgs e)
+        {
+            
+                MongoClient client = new MongoClient("mongodb://admin:a123456@ds141902.mlab.com:41902/ox");
+                MongoServer server = client.GetServer();
+                MongoDatabase database = server.GetDatabase("ox");
+                MongoCollection symbolcollection = database.GetCollection<Register2>("User");
+                Register2 register = new Register2();
+                BindingList<Register2> doclist = new BindingList<Register2>();
+                var registerDB = database.GetCollection<Register2>("User");
+                var registerDB2 = registerDB.AsQueryable().Where(pd => pd.Username == userName.Text);
+                foreach (var p in registerDB2)
+                {
+                    doclist.Add(p);
+                    Application.DoEvents();
+                }
+                dataGridView1.DataSource = doclist;
+                if (dataGridView1.Rows.Count == 0)
+                {
+                    if (!string.IsNullOrEmpty(userName.Text.Trim()))
+                    {
+                        register.Username = userName.Text;
+                    }else
+                    {
+                        MessageBox.Show("ใส่ข้อมูลให้สมบูรณ์");
+                    }
+                    if (!string.IsNullOrEmpty(password.Text.Trim()))
+                    {
+                        register.Password = password.Text;
+                    }
+                    else
+                    {
+                        MessageBox.Show("ใส่ข้อมูลให้สมบูรณ์");
+                    }
+                    if (!string.IsNullOrEmpty(idName.Text.Trim()))
+                    {
+                        register.Name = idName.Text;
+                    }
+                    else
+                    {
+                        MessageBox.Show("ใส่ข้อมูลให้สมบูรณ์");
+                    }
+                    register.Avatar = null;
+                    register.Win = 0;
+                    register.Draw = 0;
+                    register.Lose = 0;
+                if(!string.IsNullOrEmpty(userName.Text.Trim()) && !string.IsNullOrEmpty(password.Text.Trim()) && !string.IsNullOrEmpty(idName.Text.Trim()))
+                {
+                    symbolcollection.Insert(register);
+                    MessageBox.Show("Success");
+                    userName.Text = "";
+                    password.Text = "";
+                    cfpassword.Text = "";
+                    idName.Text = "";
+                }
+                else
+                {
+                    MessageBox.Show("ใส่ข้อมูลให้สมบูรณ์");
+                }
+            }
+        }
+
+        public class Register2
+        {
+            public ObjectId _id { get; set; }
+            public string Username
+            {
+                get; set;
+            }
+            public string Name
+            {
+                get; set;
+            }
+            public string Password
+            {
+                get; set;
+            }
+            public string Avatar
+            {
+                get; set;
+            }
+            public int Win
+            {
+                get; set;
+            }
+            public int Draw
+            {
+                get; set;
+            }
+            public int Lose
+            {
+                get; set;
+            }
+
+        }
+    }
+}
